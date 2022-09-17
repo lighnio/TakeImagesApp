a3ca0de2195c82e5edc770124b2cba93de851485
lighnio <charlesvas16@outlook.com>
2022-09-17 08:22:48 -0600

    saving pictures to device

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Alert, StyleSheet, Text, View, TouchableOpacity, ImageBackground} from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { Camera } from "expo-camera";
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
   // Hooks
   const [startCamera, setStartCamera] = React.useState(false);
   const [previewVisible, setPreviewVisible] = useState(false);
   const [capturedImage, setCapturedImage] = useState<any>(null);
+  const [mediaLibPermission, setMediaLibPermission] = useState(false);
 
   // Variables
   let camera: any;
@@ -24,73 +33,138 @@ export default function App() {
     }
   };
 
-  // Components
+  const __savePhoto = () => {
+    MediaLibrary.saveToLibraryAsync(capturedImage.uri).then(() => {
+      setCapturedImage(null);
+    });
+  };
 
-  const CameraPreview = ({photo}: any) => {
+  const __retakePicture = () => {
+    setCapturedImage(null);
+    setPreviewVisible(false);
+    __startCamera();
+  };
+
+  const getMediaPermission = async () =>{
+    const mediaPermission = await MediaLibrary.requestPermissionsAsync();
+    setMediaLibPermission(mediaPermission.status === 'granted');
+  }
+
+  // Components
+  const CameraPreview = ({ photo }: any) => {
     console.log(photo);
-    return(
+    return (
       <View
         style={{
-          backgroundColor: 'transparent',
-          flex: 1, 
-          width: '100%',
-          height: '100%'
-        }}>
-          <ImageBackground
-            source={{uri: photo && photo.uri}}
+          backgroundColor: "transparent",
+          flex: 1,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <ImageBackground
+          source={{ uri: photo && photo.uri }}
+          style={{
+            flex: 1,
+          }}
+        >
+          <Text
+            onPress={__retakePicture}
             style={{
-              flex: 1
-            }}>
-
-          </ImageBackground>
+              color: "#fff",
+              fontWeight: "bold",
+              fontSize: 30,
+              textAlign: "center",
+              backgroundColor: "transparent",
+              position: "absolute",
+              bottom: 0,
+              flexDirection: "row",
+            }}
+          >
+            Re Take
+          </Text>
+          {mediaLibPermission ? (
+            <Text
+              onPress={__savePhoto}
+              style={{
+                color: "#fff",
+                fontWeight: "bold",
+                fontSize: 30,
+                backgroundColor: "transparent",
+                position: "absolute",
+                bottom: 0,
+                flexDirection: "row",
+                alignSelf: "flex-end",
+              }}
+            >
+              Save Photo
+            </Text>
+          ) : (
+            <Text
+              onPress={getMediaPermission}
+              style={{
+                color: "#fff",
+                fontWeight: "bold",
+                fontSize: 30,
+                backgroundColor: "transparent",
+                position: "absolute",
+                bottom: 0,
+                flexDirection: "row",
+                alignSelf: "flex-end",
+              }}
+            >
+              Save Photo
+            </Text>
+          )}
+        </ImageBackground>
       </View>
-    )
-    
-  }
+    );
+  };
 
   const __takePicture = async () => {
     // Above line validate if the camera has permissions
-    if(!camera) return;
+    if (!camera) return;
     const photo = await camera.takePictureAsync();
     console.log(photo);
     setPreviewVisible(true);
-    setCapturedImage(photo)
-    
-  }
-
+    setCapturedImage(photo);
+  };
 
   return (
     <View style={styles.container}>
       {startCamera ? (
         <>
-         {previewVisible && capturedImage ? (
-          <CameraPreview photo={capturedImage}/>
-          
-        ):(
-          <Camera
-          style={{ flex: 1, width: "100%" }}
-          ref={(r) => {
-            camera = r;
-          }}
-        >
-  
-          <View
-            style={{
-              position: "absolute",
-              bottom: 0,
-              flexDirection: 'row',
-              flex: 1,
-              width: '100%',
-              padding: 20,
-              justifyContent: 'space-between'
-            }}>
+          {previewVisible && capturedImage ? (
+            <CameraPreview
+              photo={capturedImage}
+              savePhoto={__savePhoto}
+              retakePicture={__retakePicture}
+            />
+          ) : (
+            <Camera
+              style={{ flex: 1, width: "100%" }}
+              ref={(r) => {
+                camera = r;
+              }}
+            >
               <View
                 style={{
-                  alignSelf: 'center',
+                  position: "absolute",
+                  bottom: 0,
+                  flexDirection: "row",
                   flex: 1,
-                  alignItems: 'center'
-                }}>
-  
+                  width: "100%",
+                  padding: 20,
+                  justifyContent: "space-between",
+                }}
+              >
+                <View
+                  style={{
+                    alignSelf: "center",
+                    flex: 1,
+                    alignItems: "center",
+                  }}
+                >
                   <TouchableOpacity
                     onPress={__takePicture}
                     style={{
@@ -98,17 +172,13 @@ export default function App() {
                       height: 70,
                       bottom: 0,
                       borderRadius: 50,
-                      backgroundColor: '#fff'
-                    }}>
-  
-                  </TouchableOpacity>
-  
+                      backgroundColor: "#fff",
+                    }}
+                  ></TouchableOpacity>
+                </View>
               </View>
-  
-          </View>
-  
-          </Camera>
-        )}
+            </Camera>
+          )}
         </>
       ) : (
         <View
