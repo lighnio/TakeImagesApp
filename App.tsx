35aeb411fe6c4502bbcb024927c25e8a5e11087f
lighnio <charlesvas16@outlook.com>
2022-09-17 00:57:44 -0600

    added button to take picture, only make a console.log

--- App.tsx
+++ App.tsx
@@ -18,6 +18,11 @@ export default function App() {
     }
   };
 
+  const __takePicture = async () => {
+    console.log("TAKING PICTUREEEE");
+    
+  }
+
   return (
     <View style={styles.container}>
       {startCamera ? (
@@ -26,7 +31,42 @@ export default function App() {
           ref={(r) => {
             camera = r;
           }}
-        ></Camera>
+        >
+
+          <View
+            style={{
+              position: "absolute",
+              bottom: 0,
+              flexDirection: 'row',
+              flex: 1,
+              width: '100%',
+              padding: 20,
+              justifyContent: 'space-between'
+            }}>
+              <View
+                style={{
+                  alignSelf: 'center',
+                  flex: 1,
+                  alignItems: 'center'
+                }}>
+
+                  <TouchableOpacity
+                    onPress={__takePicture}
+                    style={{
+                      width: 70,
+                      height: 70,
+                      bottom: 0,
+                      borderRadius: 50,
+                      backgroundColor: '#fff'
+                    }}>
+
+                  </TouchableOpacity>
+
+              </View>
+
+          </View>
+
+        </Camera>
       ) : (
         <View
           style={{
