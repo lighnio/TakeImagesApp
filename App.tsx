d9ce8d811a77f82a1d767df68b6c5622c2e3e18f
lighnio <charlesvas16@outlook.com>
2022-09-16 21:58:16 -0600

    added camera view and validation for the permissions

--- App.tsx
+++ App.tsx
@@ -1,41 +1,66 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  let camera: any;
+  const [startCamera, setStartCamera] = React.useState(false);
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+
+    if (status === "granted") {
+      //Starting the camera in the above line.
+      setStartCamera(true);
+    } else {
+      Alert.alert("Access Denied");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "#fff",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <TouchableOpacity
+      {startCamera ? (
+        <Camera
+          style={{ flex: 1, width: "100%" }}
+          ref={(r) => {
+            camera = r;
+          }}
+        ></Camera>
+      ) : (
+        <View
           style={{
-            width: 130,
-            borderRadius: 4,
-            backgroundColor: "#14274e",
-            flexDirection: "row",
+            flex: 1,
+            backgroundColor: "#fff",
             justifyContent: "center",
             alignItems: "center",
-            height: 40,
           }}
         >
-          <Text
+          <TouchableOpacity
+            onPress={__startCamera}
             style={{
-              color: "#fff",
-              fontWeight: "bold",
-              textAlign: "center",
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: "#14274e",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              height: 40,
             }}
           >
-            Take Picture
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto"/>
+            <Text
+              style={{
+                color: "#fff",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Take Picture
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      <StatusBar style="auto" />
     </View>
   );
 }
