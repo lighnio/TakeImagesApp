aef7fa0a81ac2e551d4a9966c033ec8256795ce6
lighnio <charlesvas16@outlook.com>
2022-09-16 21:32:12 -0600

    Added button for take a picture

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#fff",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: "#14274e",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: 40,
+          }}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Take Picture
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
